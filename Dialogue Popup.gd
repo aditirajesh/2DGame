49f5b754fda94856218162c410ef91f854d02f8c
Aditi Rajesh <111845100+aditirajesh@users.noreply.github.com>
2024-05-12 22:54:24 +0530

    Add files via upload

---
+++ Dialogue Popup.gd
@@ -0,0 +1,7 @@
+extends Popup
+
+var nickname : set = npc_name_set 
+var message : set = message_set 
+var response : set = response_set 
+
+var npc 
---
+++ DialoguePopup.gd
@@ -0,0 +1,6 @@
+extends Popup
+
+var nickname : set = npc_name_set
+var message : set = message_set 
+var response : set = response_set 
+
